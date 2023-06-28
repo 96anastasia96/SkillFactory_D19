42aefc65d50ceeb6c92b66fa15c13565451333cb
96anastasia96 <ak96ak96@yandex.ru>
2023-06-28 15:44:51 +0300

    .

--- funproject/sign/urls.py
+++ funproject/sign/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register_user, login_view, GetCode, InvalidCode, LogoutViewCustom
+from .views import register_user, login_view, InvalidCode, LogoutViewCustom, verify_code, GetCode
 
 urlpatterns = [
     path('login/', login_view, name='login'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', register_user, name='signup'),
     path('code/<str:user>', GetCode.as_view(), name='code'),
     path('code/invalid/', InvalidCode.as_view(), name='invalid_code'),
+    path('verify_code/', verify_code, name='verify_code'),
 ]
--- funproject/sign/views.py
+++ funproject/sign/views.py
@@ -1,15 +1,13 @@
 import random
-from string import hexdigits, digits
-from allauth.account.views import LogoutView
+from string import digits
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+from django.urls import reverse
 from django.views.generic.edit import CreateView
 from funproject import settings
 from .models import OneTimeCode
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from .forms import SignUpForm
 from django.contrib.auth import authenticate, login, logout
 
@@ -19,7 +17,7 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user is not None and user.is_active:  # Проверка активности пользователя
             login(request, user)
             messages.success(request, ("You Have Been Logged In!"))
             return redirect('home')
@@ -31,6 +29,20 @@ def login_view(request):
         return render(request, "sign/templates/login.html", {})
 
 
+def verify_code(request):
+    if request.method == "POST":
+        code = request.POST['code']
+        if OneTimeCode.objects.filter(code=code, user=request.user).exists() and request.user.is_active:  # Проверка активности пользователя
+            User.objects.filter(username=request.user.username).update(is_active=True)
+            OneTimeCode.objects.filter(code=code, user=request.user).delete()
+            messages.success(request, ("Code verification successful. You can now login."))
+            return redirect('login')
+        else:
+            messages.error(request, ("Invalid code or user is not active. Please try again."))
+            return redirect('verify_code')
+
+    return render(request, "sign/templates/code.html", {})
+
 
 def register_user(request):
     form = SignUpForm()
@@ -39,7 +51,7 @@ def register_user(request):
         if form.is_valid():
             user = form.save()
             code = ''.join(random.choices(digits, k=6))
-            one_time_code = OneTimeCode(user=user, code=code)
+            one_time_code = OneTimeCode(user=user, code=code)  # Указываем пользователя при создании объекта
             one_time_code.save()
             # Отправка почты с OTP-кодом
             send_mail(
@@ -50,10 +62,9 @@ def register_user(request):
             )
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            # email = form.cleaned_data['email']
             user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, ("You have successfully registered! Welcome!"))
+
+            messages.success(request, ("Код был отправлен на вашу почту."))
             return redirect('code', user=user.username)
     return render(request, 'sign/templates/signup.html', {'form': form})
 
@@ -70,26 +81,8 @@ class GetCode(CreateView):
         context = super().get_context_data(**kwargs)
         name_user = self.kwargs.get('user')
         if not OneTimeCode.objects.filter(user=User.objects.get(username=name_user)).exists():
-            code = ''.join(random.sample(digits, k=6))
-            one_time_code = OneTimeCode(user=name_user, code=code)
-            one_time_code.save()
-            user = User.objects.get(username=name_user)
-            send_mail(
-                subject=f'Код активации',
-                message=f'Код активации аккаунта: {code}',
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[user.email],
-            )
             context['message'] = "Код был отправлен на вашу почту."
         return context
-
-    def post(self, request, *args, **kwargs):
-        if OneTimeCode.objects.filter(code=request.POST['code'], user=request.user).exists():
-            User.objects.filter(username=request.user.username).update(is_active=True)
-            OneTimeCode.objects.filter(code=request.POST['code'], user=request.user).delete()
-            return redirect('login')
-        else:
-            return render(self.request, 'sign/templates/invalid_code.html')
 
 
 class InvalidCode(CreateView):
